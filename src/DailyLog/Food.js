fba6ecfcdb7571a87e8e971344ca870932ebdf5b
Andrea Pullen <apullenb@gmail.com>
2021-02-26 19:36:49 -0600

    disp

--- src/DailyLog/Food.js
+++ src/DailyLog/Food.js
@@ -8,13 +8,29 @@ function Food() {
         const [snack, setSnack] = useState('')
         const [show, setShow] = useState('')
 
+    const onChangeB = e => setBrkf(e.target.value)
+    const close = () => {
+     show === ''   setShow('hide')
+    }
     return (
         <div className="food">
-            <p>Breakfast <button> +add </button></p>
-            <p>Lunch <button> +add </button></p>
-            <p>Dinner <button> +add </button></p>
-            <p>Snacks <button> +add </button></p>
-
+            <div className='food-box'>
+            <p>Breakfast </p>
+            <div className ={show}><input type='text' value={brkf} onChange={(e)=> onChangeB(e)}/> <button > +add </button></div>
+            </div>
+            <div className='food-box'>
+            <p>Lunch </p>
+            <div className ={show}><input type='text' value={lunch} /><button> +add </button></div>
+            </div>
+            <div className='food-box'>
+            <p>Dinner </p>
+            <div className ={show}><input type='text' value={din} /><button> +add </button></div>
+            </div>
+            <div className='food-box'>
+            <p>Snacks </p>
+            <div className ={show}><input type='text' value={snack} /><button> +add </button></div>
+            </div>
+            <button style={{margin:'7px 15px'}} onClick={close}> x close</button>
         </div>
     )
 }
